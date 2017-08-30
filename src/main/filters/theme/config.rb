0d7b20d7b5902f304ace3bafa5a012ff17a3c665
Freddy Barrera <freddy.barrera.moo@gmail.com>
2017-08-30 08:17:31 -0500

    Cambie el groupId y el artifactId del proyecto, y actualice las rutas para que sean congruentes con el cambie de groupId y el artifactId

--- src/main/filters/theme/config.rb
+++ src/main/filters/theme/config.rb
@@ -3,7 +3,7 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "../../../main/resources/META-INF/resources/primefaces-saludtlax-modena/"
+css_dir = "../../../main/resources/META-INF/resources/primefaces-siayf-modena/"
 sass_dir = "scss"
 images_dir = "imagenes"
 javascripts_dir = "js"
